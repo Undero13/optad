943cfecb1440a1911be36bb0f317ffea9128fcb1
Patryk Loter <patryk.loter@gmail.com>
2020-07-29 18:56:15 +0100

    add code linters and code lint

--- app/src/Command/GetDataCommand.php
+++ app/src/Command/GetDataCommand.php
@@ -8,41 +8,40 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class GetDataCommand extends Command 
+class GetDataCommand extends Command
 {
-  protected static $defaultName = 'app:getdata';
-  private $dataManager;
-  private $url;
+    protected static $defaultName = 'app:getdata';
+    private $dataManager;
 
-  public function __construct(DataManager $dataManager)
-  {
-    $this->dataManager = $dataManager;
+    public function __construct(DataManager $dataManager)
+    {
+        $this->dataManager = $dataManager;
 
-    parent::__construct();
-  }
+        parent::__construct();
+    }
  
-  protected function configure()
-  {
-    $this
-      ->setDescription('Get data from company api')
-      ->setHelp('This command allows you get and save data from your company partners')
-      ->addArgument('url', InputArgument::OPTIONAL, 'api url');
-  }
-
-  protected function execute(InputInterface $input, OutputInterface $output)
-  {
-    $url = $input->getArgument('url') ?? 'https://api.optad360.com/testapi';
-    $output->writeln('<question>Api url is: '. $url . '</question>');
-
-    try {
-      $this->dataManager->getData($url);
-      $this->dataManager->saveData();
-    } catch (\Exception $e) {
-      $output->writeln('<error>' . $e->getMessage() . '</error>');
-      return Command::FAILURE;
+    protected function configure()
+    {
+        $this
+        ->setDescription('Get data from company api')
+        ->setHelp('This command allows you get and save data from your company partners')
+        ->addArgument('url', InputArgument::OPTIONAL, 'api url');
     }
 
-    $output->writeln('<info> Data add to database </info>');
-    return Command::SUCCESS;
-  }
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $url = $input->getArgument('url') ?? 'https://api.optad360.com/testapi';
+        $output->writeln('<question>Api url is: '. $url . '</question>');
+
+        try {
+            $this->dataManager->getData($url);
+            $this->dataManager->saveData();
+        } catch (\Exception $e) {
+            $output->writeln('<error>' . $e->getMessage() . '</error>');
+            return Command::FAILURE;
+        }
+
+        $output->writeln('<info> Data add to database </info>');
+        return Command::SUCCESS;
+    }
 }
--- app/src/Service/DataManager.php
+++ app/src/Service/DataManager.php
@@ -2,73 +2,72 @@
 namespace App\Service;
 
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
-class DataManager 
+class DataManager
 {
 
-  private $data;
-  private $client;
-  private $serializer;
+    private $data;
+    private $client;
+    private $serializer;
 
-  public function __construct(HttpClientInterface $client, EntityManagerInterface $em, SerializerInterface $serializer)
-  {
-    $this->em = $em;
-    $this->client = $client;
-    $this->serializer = $serializer;
-  }
-
-  public function getData(string $url)
-  {
-    try {
-      $response = $this->client->request("GET", $url);
-    } catch (\Exception $e) {
-      throw new \Exception($e->getMessage());
+    public function __construct(HttpClientInterface $client, EntityManagerInterface $em, SerializerInterface $serializer)
+    {
+        $this->em = $em;
+        $this->client = $client;
+        $this->serializer = $serializer;
     }
+
+    public function getData(string $url)
+    {
+        try {
+            $response = $this->client->request("GET", $url);
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
     
-    $statusCode = $response->getStatusCode();
+        $statusCode = $response->getStatusCode();
 
-    if ($statusCode === 200) {
-      $this->data = $response->toArray();
-    } else {
-      throw new \Exception('Somethink wrong. Response status:' . $statusCode);
+        if ($statusCode === 200) {
+            $this->data = $response->toArray();
+        } else {
+            throw new \Exception('Somethink wrong. Response status:' . $statusCode);
+        }
     }
-  }
 
-  public function saveData()
-  {
-      list($modelName, $data) = $this->processData();
+    public function saveData()
+    {
+        list($modelName, $data) = $this->processData();
 
-      $type = "App\\Entity\\$modelName";
+        $type = "App\\Entity\\$modelName";
 
-      foreach ($data as $row) {
-        $model = $this->serializer->deserialize(json_encode($row), $type, 'json');
-        $this->em->persist($model);
-        $this->em->flush();
-      }
-  }
+        foreach ($data as $row) {
+            $model = $this->serializer->deserialize(json_encode($row), $type, 'json');
+            $this->em->persist($model);
+            $this->em->flush();
+        }
+    }
 
-  private function processData(): array
-  {
-    $name = $this->data['settings']['currency'];
-    $headers = $this->normalizeHeaders($this->data['headers']);
-    $assocData = [];
+    private function processData(): array
+    {
+        $name = $this->data['settings']['currency'];
+        $headers = $this->normalizeHeaders($this->data['headers']);
+        $assocData = [];
     
-    foreach ($this->data['data'] as $row) {
-      array_push($assocData,array_combine($headers,$row));
+        foreach ($this->data['data'] as $row) {
+            array_push($assocData, array_combine($headers, $row));
+        }
+
+        return [$name, $assocData];
     }
 
-    return [$name, $assocData];
-  }
-
-  private function normalizeHeaders(array $headers): array
-  {
-    $callback = function(string $value) { 
-      return str_replace(" ", "_", strtolower($value)); 
-    };
+    private function normalizeHeaders(array $headers): array
+    {
+        $callback = function (string $value) {
+            return str_replace(" ", "_", strtolower($value));
+        };
   
-    return array_map($callback, $headers);
-  }
+        return array_map($callback, $headers);
+    }
 }
